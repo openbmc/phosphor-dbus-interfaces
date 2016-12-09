3242a28eed4c9935e8b504445a98b3ff095b4c5c
Patrick Williams <patrick@stwcx.xyz>
2016-12-09 14:30:45 -0600

    build: autotools files for libphosphor_dbus.so
    Use the interface YAML files present to generate the server.cpp
    and server.hpp files, build them into a shared library, and
    install.
    
    Change-Id: Ib08704c399975f42aa77f911a7bc7dc33bacba99
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

---
+++ generate_makefile.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+toplevel_dirs=xyz
+interfaces=`find $toplevel_dirs -name "*.interface.yaml"`
+
+for i in ${interfaces};
+do
+    iface=`echo $i | sed s/\\.interface.yaml// | sed "s/\//./g"`
+    cat <<MAKEFILE
+
+${i%.interface.yaml}/server.cpp: ${i} ${i%.interface.yaml}/server.hpp
+	@mkdir -p \`dirname \$@\`
+	sdbus++ -r \$(srcdir) interface server-cpp ${iface} > \$@
+
+${i%.interface.yaml}/server.hpp: ${i}
+	@mkdir -p \`dirname \$@\`
+	sdbus++ -r \$(srcdir) interface server-header ${iface} > \$@
+
+MAKEFILE
+
+done
+
+echo "libphosphor_dbus_cpp_SOURCES = \\"
+for i in ${interfaces};
+do
+    echo "	${i%.interface.yaml}/server.cpp \\"
+done
+echo
+
+echo "libphosphor_dbus_hpp_SOURCES = \\"
+for i in ${interfaces};
+do
+    echo "	${i%.interface.yaml}/server.hpp \\"
+done
+echo
+
+cat << MAKEFILE
+libphosphor_dbus.cpp: \$(libphosphor_dbus_cpp_SOURCES)
+	cat \$^ > \$@
+
+nobase_include_HEADERS = \$(libphosphor_dbus_hpp_SOURCES)
+
+.PHONY: clean-dbus
+clean-dbus:
+	for i in \$(libphosphor_dbus_cpp_SOURCES) \\
+	         \$(libphosphor_dbus_hpp_SOURCES); \\
+	do \\
+	    test -e \$\$i && rm \$\$i ; \\
+	    test -d \`dirname \$\$i\` && rmdir -p \`dirname \$\$i\` ; \\
+	    true; \\
+	done
+MAKEFILE

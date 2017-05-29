3c18ea4150879cfed659705fc1e3606b65c61b70
Marri Devender Rao <devenrao@in.ibm.com>
2017-05-28 19:31:00 -0500

    phosphor-dbus: copy error yaml files to shared location
    Changes done to the scripts and Makefile to copy the
    error yaml files to a shared location.
    
    Change-Id: Ibcaec08504e31aa885ae7e3e09704ba9e9abaddd
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- generate_interface_yaml_makefile.sh
+++ generate_interface_yaml_makefile.sh
@@ -3,7 +3,7 @@
 cd $1
 
 toplevel_dirs=xyz
-all_yaml=`find $toplevel_dirs -name "*.yaml"`
+all_yaml=`find $toplevel_dirs -name "*interface.yaml"`
 
 echo "nobase_yaml_DATA = \\"
 for i in ${all_yaml};

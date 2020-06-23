935a449ced4669f2140c605c063cccfb3013a529
William A. Kennington III <wak@google.com>
2020-06-23 02:54:24 -0700

    build: enable meson build
    Add meson build support.  Autotools support will be removed in
    the future once the consuming meta-phosphor recipes are updated.
    
    This should be feature complete relative to the previous autotools
    support.
    
    Additionally, support is added for using sdbusplus as a subproject
    in the event that users do not have sdbusplus installed.
    
    Change-Id: If822cea3c395618d47e43ad3feee8fc3d3c68b5e
    Signed-off-by: William A. Kennington III <wak@google.com>

--- README.md
+++ README.md
@@ -2,15 +2,24 @@
 YAML descriptors of standard D-Bus interfaces.
 The format is described by the [sdbusplus binding generation tool sdbus++][].
 
-Only the xyz/openbmc_project interfaces are built by default.
+## Building
 
-Enable the OpenPower D-Bus interfaces with:
-```
---enable-openpower-dbus-interfaces
-```
-Enable IBM D-Bus interfaces with:
-```
---enable-ibm-dbus-interfaces
-```
+This project can be built with `meson`.  The typical `meson` workflow is:
+`meson builddir && cd builddir && ninja`.
+
+Due to the limited code generation methods available in meson today, changes
+to YAML files are not tracked by `meson` and do not automatically cause a new
+recompile.  Subsequent runs when making YAML changes will require running
+`ninja reconfigure` in order to regenerate the code files from the YAML.
+
+## Configuration
+
+Only the xyz/openbmc_project interfaces are built by default.  Other interfaces
+can be enabled by meson options:
+
+- com/ibm - `-Ddata_com_ibm=true`
+- org/open_power - `-Ddata_org_open_power=true`
+
+## References
 
 [sdbusplus binding generation tool sdbus++]: https://github.com/openbmc/sdbusplus/blob/master/README.md#binding-generation-tool
---
+++ gen.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+declare -A visited
+append_clean() {
+  local file="$1"
+
+  if [ -z "${visited["$file"]-}" ]; then
+    rm -rf "$file"
+    visited["$file"]=1
+  fi
+
+  mkdir -p "$(dirname "$file")"
+  cat >>"$file"
+}
+
+for file in $(find com xyz org -name '*.yaml'); do
+  file="${file#./}"
+  path="${file%.*.yaml}"
+  domain="${path//\//.}"
+  meson="$path/meson.build"
+
+  if [[ "$file" == *.interface.yaml ]]; then
+    append_clean "$meson" <<EOF
+generated_compile += custom_target(
+  'server.cpp',
+  output: 'server.cpp',
+  input: '../$(basename "$file")',
+  capture: true,
+  command: [sdbusplusplus_prog, '-r', phosphor_dbus_root, 'interface', 'server-cpp', '$domain'])
+
+generated_compile += custom_target(
+  'server.hpp',
+  output: 'server.hpp',
+  input: '../$(basename "$file")',
+  capture: true,
+  command: [sdbusplusplus_prog, '-r', phosphor_dbus_root, 'interface', 'server-header', '$domain'],
+  install: true,
+  install_dir: get_option('includedir') / '$path')
+
+EOF
+  fi
+
+  if [[ "$file" == *.errors.yaml ]]; then
+    append_clean "$meson" <<EOF
+generated_compile += custom_target(
+  'error.cpp',
+  output: 'error.cpp',
+  input: '../$(basename "$file")',
+  capture: true,
+  command: [sdbusplusplus_prog, '-r', phosphor_dbus_root, 'error', 'exception-cpp', '$domain'])
+
+generated_compile += custom_target(
+  'error.hpp',
+  output: 'error.hpp',
+  input: '../$(basename "$file")',
+  capture: true,
+  command: [sdbusplusplus_prog, '-r', phosphor_dbus_root, 'error', 'exception-header', '$domain'],
+  install: true,
+  install_dir: get_option('includedir') / '$path')
+
+EOF
+  fi
+done
+
+meson_dirs=()
+for file in "${!visited[@]}"; do
+  meson_dirs+=("$(dirname "$file")")
+done
+declare -A written=()
+while [ "${#meson_dirs[@]}" -gt 0 ]; do
+  declare -A new_dirs=()
+  for dir in "${meson_dirs[@]}"; do
+    parent="$(dirname "$dir")"
+    if [ -n "${written["$dir"]-}" ]; then
+      continue
+    fi
+    if [ "$(dirname "$parent")" = '.' ]; then
+      continue
+    fi
+    append_clean "$parent/meson.build" <<EOF
+subdir('$(basename "$dir")')
+EOF
+    written["$dir"]=1
+    new_dirs["$parent"]=1
+  done
+  meson_dirs=("${!new_dirs[@]}")
+done
